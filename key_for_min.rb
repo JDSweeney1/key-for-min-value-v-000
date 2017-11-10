c484e9751af78937aeae3e01e5aab031e3b01e0d
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-10 02:49:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   value = nil
   name_hash.each do |k, v|
-    if value = nil || v < value
+    if value == nil || v < value
       value = v
       key = k
     end
